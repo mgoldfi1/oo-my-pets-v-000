e7ec81b24561ad9c43726e00a8daf58f1671187f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-28 13:33:32 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,21 @@
 class Owner
 attr_accessor :name, :pets
 attr_reader :species
+@@all = []
 
 def initialize(species)
   @species = species
   @pets = {fishes: [], cats: [], dogs: []}
+  @@all << self
 end
 
 def say_species
   puts self.species
 end
 
+def self.all
+  @@all
+end
 
 
 end
